73869a69e19a35209c141352f99b0957f73071f8
Viknesh <vickygamingoff2004@gmail.com>
2024-05-12 20:24:08 +0530

    Almost Completed

--- Home.js
+++ Home.js
@@ -4,14 +4,46 @@ import { Button } from "react-native";
 
 export default function Home({route, navigation}) {
 
+    const [data, setData] = useState('');
+
+    getDetails = () => {
+        fetch(`http://192.168.29.171:8989/get-all-data/${route.params.register_number}`)
+      .then(response => response.json())
+      .then(setData);
+    };
+
     useEffect(() => {
+        getDetails();
     }, []);
 
     return(
         <View style={styles.container}>
-            <Text multiline={false} style={styles.text}>{route.params.register_number}</Text>
-            <Text multiline={false} style={styles.text}>{route.params.title}</Text>
-            <Text multiline={false} style={styles.text}>{route.params.name}</Text>
+            <View style={styles.row}>
+                <Text multiline={false} style={[styles.text, styles.text_hotizontal]}>{data['degree']}</Text>
+                <Text multiline={false} style={[styles.text, styles.text_hotizontal]}>{data['semester']}</Text>
+            </View>
+            <Text multiline={false} style={styles.text}>{data['department']}</Text>
+            <View style={styles.row}>
+                <Text multiline={false} style={[styles.text, styles.text_hotizontal]}>{data['dob']}</Text>
+                <Text multiline={false} style={[styles.text, styles.text_hotizontal]}>{data['doj']}</Text>
+            </View>
+            <Text multiline={false} style={styles.text}>{data['address']}</Text>
+            <View style={styles.row}>
+                <Text multiline={false} style={[styles.text, styles.text_hotizontal]}>{data['mark-10']}</Text>
+                <Text multiline={false} style={[styles.text, styles.text_hotizontal]}>{data['mark-12']}</Text>
+            </View>
+            <View style={styles.row}>
+                <Text multiline={false} style={[styles.text, styles.text_hotizontal]}>{data['register_number']}</Text>
+                <Text multiline={false} style={[styles.text, styles.text_hotizontal]}>{data['cutoff']}</Text>
+            </View>
+            <View style={styles.row}>
+                <Text multiline={false} style={[styles.text, styles.text_hotizontal]}>{data['country-code']}</Text>
+                <Text multiline={false} style={[styles.text, styles.text_hotizontal]}>{data['mobile']}</Text>
+            </View>
+            <View style={styles.row}>
+                <Text multiline={false} style={[styles.text, styles.text_hotizontal]}>{data['days-hosteller']}</Text>
+                <Text multiline={false} style={[styles.text, styles.text_hotizontal]}>{data['quota']}</Text>
+            </View>
         </View>
     );
 }
@@ -21,12 +53,28 @@ const styles = StyleSheet.create({
         flex: 1,
         fontSize:14,
         flexDirection: 'column',
-        padding:10
+        padding:10,
+        backgroundColor: 'rgba(0, 0, 0, 0.9)'
       },
       text: {
         alignContent: 'center',
         justifyContent: 'center',
         marginVertical: 5,
-        color: "#000",
+        color: "#fff",
+        borderColor: "#fff" ,
+        padding: 5,
+        borderWidth: 1,
+        borderRadius: 5,
       },
+      text_hotizontal: {
+        flex: 0.5,
+        flexDirection: 'row',
+        marginHorizontal: 2.5,
+      },
+      row: {
+        flexDirection: 'row',
+        alignItems: 'stretch',
+        alignContent: 'space-between',
+        justifyContent: 'space-between'
+      }
 });
--- Login.js
+++ Login.js
@@ -31,23 +31,10 @@ export default function Login({route, navigation}) {
                   const data = await this.authenticate();
                   setStudentName(data['username_real']);
                   if(data['password_real'] == passInput) {
+                    console.log(data);
                     navigation.navigate('Home', {
                       title: student_name,
-                      address: data['address'],
-                      country_code: data['country-code'],
-                      cutoff: data['cutoff'],
-                      days_hosteller: data['days-hosteller'],
-                      degree: data['degree'],
-                      department: data['department'],
-                      dob: data['dob'],
-                      doj: data['doj'],
-                      mark_10: data['mark-10'],
-                      mark_12: data['mark-12'],
-                      mobile: data['mobile'],
-                      name: data['name'],
-                      quota: data['quota'],
-                      register_number: data['register_number'],
-                      semester: data['semester']
+                      register_number: number
                     });
                   } else {
                     Alert.alert('Warning', 'Wrong Password');
